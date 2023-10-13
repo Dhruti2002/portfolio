652446f2bb990923a4f8bfb83ac3fc8227f6fbcc
Dhruti2002 <132155248+Dhruti2002@users.noreply.github.com>
2023-10-13 12:35:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript source code
+$('.portfolio-button').mousedown(function () {
+    timeout = setInterval(function () {
+        window.scrollBy(0, -1); // May need to be -1 to go down
+    }, 0); // Play around with this number. May go too fast
+
+    return false;
+});
